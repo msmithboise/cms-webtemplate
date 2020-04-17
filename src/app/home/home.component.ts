8a2735f506f0fe88fc43443de84bd85b81d6b7b3
Mike Smith <msmithboise@gmail.com>
2020-04-17 14:29:01 -0600

    dashboard page added!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { TextLayerComponent } from 'src/app/text-layer/text-layer.component';
-import { BannerComponent } from 'src/app/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
